4957f3f6e513dde73a1ecc2c652471bd41e1b946
Soumen <souman.trivedi@ordnancesurvey.co.uk>
2014-11-14 16:46:04 +0000

    Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,9 +23,9 @@
 #      Default: /dev
 #
 #    [*polling_interval*]
-#      Specifies the interval between two path checks in seconds. 
-#      For properly functioning paths, the interval between checks will gradually 
-#      increase to (4 * polling_interval). The default value is 5. 
+#      Specifies the interval between two path checks in seconds.
+#      For properly functioning paths, the interval between checks will gradually
+#      increase to (4 * polling_interval). The default value is 5.
 #      Default: 5
 #
 #    [*path_selector*]
@@ -36,7 +36,7 @@
 #
 #    [*path_grouping_policy*]
 #      Specifies the default path grouping policy to apply to unspecified multipaths.
-#      Possible values include: 
+#      Possible values include:
 #        failover, multibus, group_by_serial, group_by_prio, group_by_node_name
 #      Default: failover
 #
@@ -46,17 +46,17 @@
 #
 #    [*path_checker*]
 #      Specifies the default method used to determine the state of the paths
-#      Default: directio
+#      Default: tur (Test Unit Ready)
 #
 #    [*rr_min_io*]
-#      Specifies the number of I/O requests to route to a path before switching 
+#      Specifies the number of I/O requests to route to a path before switching
 #      to the next path in the current path group
 #      Default: 1000
 #
 #    [*rr_weight*]
-#      If set to priorities, then instead of sending rr_min_io requests to a 
-#      path before calling path_selector to choose the next path, the number 
-#      of requests to send is determined by rr_min_io times the path's priority, 
+#      If set to priorities, then instead of sending rr_min_io requests to a
+#      path before calling path_selector to choose the next path, the number
+#      of requests to send is determined by rr_min_io times the path's priority,
 #      as determined by the prio function. If set to uniform, all path weights are equal.
 #      Default: uniform.
 #
@@ -65,7 +65,7 @@
 #      Default: manual
 #
 #    [*no_path_retry*]
-#      A numeric value for this attribute specifies the number of times the system should 
+#      A numeric value for this attribute specifies the number of times the system should
 #      attempt to use a failed path before disabling queueing.
 #      Default: fail
 #
@@ -79,42 +79,32 @@
 #      (This will avoid blacklisting of local devices manualy)
 #      Default: yes
 #
-class multipath(
+class multipath (
   $enable               = true,
   $ensure               = true,
   $installed            = true,
-  $udev_dir             = "/dev",
-  $polling_interval     = "10",
-  $path_selector        = "\"round-robin 0\"",
-  $path_grouping_policy = 'multibus',
-  $prio                 = "const",
-  $path_checker         = "directio",
-  $rr_min_io            = "1000",
-  $rr_weight            = "uniform",
-  $failback             = "manual",
-  $no_path_retry        = "fail",
+  $polling_interval     = "30",
+  $path_grouping_policy = "group_by_prio",
+  $path_checker         = "tur",
+  $rr_min_io            = "100",
+  $failback             = "immediate",
+  $no_path_retry        = "queue",
   $user_friendly_names  = 'yes',
-  $find_multipaths      = 'yes'
-){
+  $find_multipaths      = 'no') {
   if ($is_virtual == 'false') and ($kernel == 'Linux') {
-
+    package { "redhat-lsb": ensure => "installed" }
     validate_bool($enable)
     validate_bool($ensure)
     validate_bool($installed)
 
-    validate_re($udev_dir, '^\/')
+    # validate_re($udev_dir, '^\/')
     validate_re($path_grouping_policy, '^failover$|^multibus$|^group_by_serial$|^group_by_prio$|^group_by_node_name$')
-    validate_re($prio, '^const$|^emc$|^alua$|^tpg_pref$|^ontap$|^rdac$|^hp_sw$|^hds$')
+    # validate_re($prio, '^const$|^emc$|^alua$|^tpg_pref$|^ontap$|^rdac$|^hp_sw$|^hds$')
     validate_re($path_checker, '^tur$|^readsector0$|^emc_clariion$|^hp_sw$|^rdac$|^directio$')
     validate_re($failback, '^manual$|^immediate$|^followover$')
     validate_re($no_path_retry, '^fail$|^queue$')
     validate_re($user_friendly_names, '^yes$|^no$')
     validate_re($find_multipaths, '^yes$|^no$')
-      
-    $getuid_callout = $lsbmajdistrelease ? {
-      '5' => "\"/sbin/scsi_id -g -u -s /block/%n\"",
-      '6' => "\"/lib/udev/scsi_id --whitelisted --device=/dev/%n\"",
-    }
 
     $ensure_real = $ensure ? {
       true  => 'running',
@@ -125,28 +115,26 @@ class multipath(
       true  => 'present',
       false => 'absent',
     }
-      
+
     $package_name = $lsbmajdistrelease ? {
       default => "device-mapper-multipath",
     }
-      
+
     $mpath_config_file = $lsbmajdistrelease ? {
       default => "/etc/multipath.conf",
     }
-     
+
     $mpath_template = $lsbmajdistrelease ? {
       default => template("${module_name}/multipath.conf.erb"),
     }
-      
+
     $service_name = $lsbmajdistrelease ? {
       default => "multipathd",
     }
-      
-    package {$package_name:
-      ensure => $installed_real,
-    }
-     
-    file {$mpath_config_file:
+
+    package { $package_name: ensure => $installed_real, }
+
+    file { $mpath_config_file:
       ensure  => $installed ? {
         true  => present,
         false => absent,
@@ -156,17 +144,18 @@ class multipath(
       mode    => '0644',
       content => $mpath_template,
     }
-      
-    service {$service_name:
+
+    service { $service_name:
       ensure     => $ensure_real,
       enable     => $enable,
       hasstatus  => true,
       hasrestart => true,
     }
-    
-    Package[$package_name] -> 
-    File[$mpath_config_file] ~> 
+
+    Package['redhat-lsb'] ->
+    Package[$package_name] ->
+    File[$mpath_config_file] ~>
     Service[$service_name]
   }
 
-}# End Class
+} # End Class
